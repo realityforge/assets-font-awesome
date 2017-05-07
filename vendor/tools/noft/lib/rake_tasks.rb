df0e316bcc9736eb423585252ec40427b95a04f2
Peter Donald <peter@realityforge.org>
2017-05-08 03:44:57 +1000

    Use latest generators version

--- vendor/tools/noft/lib/rake_tasks.rb
+++ vendor/tools/noft/lib/rake_tasks.rb
@@ -57,163 +57,43 @@ module Noft #nodoc
     end
   end
 
-  class GenerateTask
-    attr_accessor :description
-    attr_accessor :namespace_key
-    attr_accessor :filter
-    attr_writer :verbose
-
-    attr_reader :repository_key
-    attr_reader :key
-    attr_reader :generator_keys
-    attr_reader :target_dir
-
-    attr_reader :task_name
-
-    def initialize(repository_key, key, generator_keys, target_dir, buildr_project = nil)
-      @repository_key, @key, @generator_keys, @buildr_project =
-        repository_key, key, generator_keys, buildr_project
-      @namespace_key = :resgen
-      @filter = nil
-      @template_map = {}
-      # Turn on verbose messages if buildr is turned on tracing
-      @verbose = trace?
-      @target_dir = target_dir
-      yield self if block_given?
-      define
-      @templates = Noft::Generator.generator.load_templates_from_template_sets(generator_keys)
-      Reality::Generators::Buildr.configure_buildr_project(buildr_project, task_name, @templates, target_dir)
+  class GenerateTask < Reality::Generators::Rake::BaseGenerateTask
+    def initialize(icon_set_key, key, generator_keys, target_dir, buildr_project = nil)
+      super(icon_set_key, key, generator_keys, target_dir, buildr_project)
     end
 
-    private
+    protected
 
-    def verbose?
-      !!@verbose
+    def default_namespace_key
+      :noft
     end
 
-    def full_task_name
-      "#{self.namespace_key}:#{self.key}"
+    def generator_container
+      Noft::Generator
     end
 
-    def define
-      desc self.description || "Generates the #{key} artifacts."
-      namespace self.namespace_key do
-        t = task self.key => ["#{self.namespace_key}:load"] do
-          begin
+    def instance_container
+      Noft
+    end
 
-            repository = nil
-            if self.repository_key
-              repository = Noft.repository_by_name(self.repository_key)
-              if Noft.repositories.size == 1
-                Noft.warn("Noft task #{full_task_name} specifies a repository_key parameter but it can be be derived as there is only a single repository. The parameter should be removed.")
-              end
-            elsif self.repository_key.nil?
-              repositories = Noft.repositories
-              if repositories.size == 1
-                repository = repositories[0]
-              else
-                Noft.error("Noft task #{full_task_name} does not specify a repository_key parameter and it can not be derived. Candidate repositories include #{repositories.collect { |r| r.name }.inspect}")
-              end
-            end
+    def root_element_type
+      :icon_set
+    end
 
-            repository.send(:extension_point, :scan_if_required)
-            repository.send(:extension_point, :validate)
-
-            Reality::Logging.set_levels(verbose? ? ::Logger::DEBUG : ::Logger::WARN,
-                                        Noft::Logger,
-                                        Reality::Generators::Logger,
-                                        Reality::Facets::Logger) do
-              Noft.info "Generator started: Generating #{self.generator_keys.inspect}"
-              Noft::Generator.generator.generate(:repository,
-                                                   repository,
-                                                   self.target_dir,
-                                                   @templates,
-                                                   self.filter)
-            end
-          rescue Reality::Generators::GeneratorError => e
-            puts e.message
-            if e.cause
-              puts e.cause.class.name.to_s
-              puts e.cause.backtrace.join("\n")
-            end
-            raise e.message
-          end
-        end
-        @task_name = t.name
-        Noft::TaskRegistry.get_aggregate_task(self.namespace_key).enhance([t.name])
-      end
+    def log_container
+      Noft
     end
   end
 
-  class LoadResourceDescriptor
-    attr_accessor :description
-    attr_accessor :namespace_key
-    attr_writer :verbose
+  class LoadResourceDescriptor < Reality::Generators::Rake::BaseLoadDescriptor
+    protected
 
-    attr_reader :filename
-
-    def initialize(filename)
-      @filename = filename
-      @namespace_key = :resgen
-      yield self if block_given?
-      define
+    def default_namespace_key
+      :noft
     end
 
-    private
-
-    def verbose?
-      !!@verbose
-    end
-
-    def define
-      namespace self.namespace_key do
-        task :preload
-
-        task :postload
-
-        desc self.description
-        task :load => [:preload, self.filename] do
-          begin
-            Noft.current_filename = self.filename
-            Reality::Logging.set_levels(verbose? ? ::Logger::DEBUG : ::Logger::WARN,
-                                        Noft::Logger,
-                                        Reality::Generators::Logger,
-                                        Reality::Facets::Logger) do
-
-              require self.filename
-            end
-          rescue Exception => e
-            print "An error occurred loading repository\n"
-            puts $!
-            puts $@
-            raise e
-          ensure
-            Noft.current_filename = nil
-          end
-          task("#{self.namespace_key}:postload").invoke
-        end
-        Noft::TaskRegistry.get_aggregate_task(self.namespace_key)
-      end
-    end
-  end
-
-  class TaskRegistry
-    class << self
-      def get_aggregate_task(namespace)
-        all_task = namespace_tasks[namespace.to_s]
-        unless all_task
-          desc "Generate all #{namespace} artifacts"
-          all_task = task('all')
-          namespace_tasks[namespace.to_s] = all_task
-        end
-        all_task
-      end
-
-      private
-
-      def namespace_tasks
-        @namespace_tasks ||= {}
-      end
+    def log_container
+      Noft
     end
   end
 end
