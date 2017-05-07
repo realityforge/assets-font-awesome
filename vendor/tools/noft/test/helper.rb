ed8c4be4bc0eb667c0725578f6ce4e9387f043b3
Peter Donald <peter@realityforge.org>
2017-05-07 14:54:22 +1000

    Make sure package.json is setup for tests

--- vendor/tools/noft/test/helper.rb
+++ vendor/tools/noft/test/helper.rb
@@ -16,15 +16,32 @@ class Noft::TestCase < Minitest::Test
   include Test::Unit::Assertions
 
   def setup
-    @base_temp_dir ||= ENV['TEST_TMP_DIR'] || File.expand_path("#{File.dirname(__FILE__)}/../tmp")
-    @temp_dir = "#{@base_temp_dir}/#{name}"
-    FileUtils.mkdir_p @temp_dir
+    @cwd = Dir.pwd
+
+    FileUtils.mkdir_p self.working_dir
+    Dir.chdir(self.working_dir)
+    File.write("#{self.working_dir}/package.json", package_json)
+
+    self.setup_node_modules
 
     Noft.reset
   end
 
   def teardown
-    FileUtils.rm_rf @base_temp_dir if File.exist?(@base_temp_dir)
+    self.unlink_node_modules
+    Dir.chdir(@cwd)
+    FileUtils.rm_rf self.working_dir if File.exist?(self.working_dir)
+  end
+
+  def setup_node_modules
+    node_modules_present = File.exist?(self.node_modules_dir)
+    FileUtils.mkdir_p self.node_modules_dir
+    FileUtils.link(self.node_modules_dir, "#{self.working_dir}/node_modules")
+    system('npm install') unless node_modules_present
+  end
+
+  def unlink_node_modules
+    FileUtils.rm("#{self.working_dir}/node_modules")
   end
 
   def create_file(filename, content)
@@ -39,6 +56,30 @@ class Noft::TestCase < Minitest::Test
   end
 
   def working_dir
-    @temp_dir
+    @working_dir ||= "#{workspace_dir}/#{SecureRandom.hex}"
+  end
+
+  def workspace_dir
+    @workspace_dir ||= ENV['TEST_TMP_DIR'] || File.expand_path("#{File.dirname(__FILE__)}/../tmp/workspace")
+  end
+
+  def node_modules_dir
+    @node_modules_dir ||= "#{workspace_dir}/node_modules"
+  end
+
+  def package_json
+    <<JSON
+{
+  "name": "test-project",
+  "version": "1.0.0",
+  "description": "A project used to test",
+  "author": "",
+  "license": "Apache-2.0",
+  "repository": ".",
+  "devDependencies": {
+    "font-blast": "^0.6.1"
+  }
+}
+JSON
   end
 end
